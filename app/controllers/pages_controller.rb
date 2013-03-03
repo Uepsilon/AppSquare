5cbace74c1f0915a689ff613689381eda1959f9b
Uepsilon <floopone@gmail.com>
2013-03-03 12:18:59 +0100

    added basic channel management, logout-link and cleaned up devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
 
   before_filter :authenticate_user!
+  before_filter :load_channel
 
   def index
 
+  end
+
+  protected
+
+  def load_channel
+    @channel = current_user.channel unless current_user.channel.empty?
   end
 end
