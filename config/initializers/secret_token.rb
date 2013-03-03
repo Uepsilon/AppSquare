5b52a9d2f29104c556ad6329354aaaab587e3986
Uepsilon <floopone@gmail.com>
2013-03-03 01:14:24 +0100

    proj init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appsquare::Application.config.secret_token = '3d43fe031740a84310d10b0feac6b5ed73b6145e5bb0ea720c14c87ff729ddf2926b0319906e4044c4b5943037f315454d597ee75840fe1488f6b03fb6bf1abe'
